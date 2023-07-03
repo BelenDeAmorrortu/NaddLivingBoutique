c679514a507e14b0527e3049bc2811ff4f3be4a9
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2023-07-03 10:25:48 -0300

    feat: new page and product metadata

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,38 +1,57 @@
-import { Nav, Footer, BackToTop} from "../../components"
-import { Montserrat, Cormorant_Garamond } from 'next/font/google'
-import './globals.css'
+import { Nav, Footer, BackToTop } from "../../components";
+import { Montserrat, Cormorant_Garamond } from "next/font/google";
+import "./globals.css";
 
 const montserrat = Montserrat({
-	weight: ['300', '400','600', '700'],
-	style: ['normal', 'italic'],
-	variable: '--font-primary',
-	subsets: ['latin'],
-	display: 'swap',
+  weight: ["300", "400", "600", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-primary",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const cormorant = Cormorant_Garamond({
-	weight: ['300', '400','600', '700'],
-	style: ['normal', 'italic'],
-	variable: '--font-secondary',
-	subsets: ['latin'],
-	display: 'swap',
+  weight: ["300", "400", "600", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-secondary",
+  subsets: ["latin"],
+  display: "swap",
 });
 
-export default async function RootLayout({ children }: {children: React.ReactNode}) {
-	
-	return (
-		<html lang="en" className={`overflow-x-hidden ${montserrat.variable} ${cormorant.variable}`}>
-			<head>
-				<title>NADD - Living Boutique</title>
-			</head>
-			<body className='overflow-x-hidden'>
-				<main>
-					<Nav/>
-					{children}
-					<Footer/>
-					<BackToTop/>
-				</main>
-			</body>
-		</html>
-	)
+export const metadata = {
+  // metadataBase: new URL('URL DEL DEPLOY'),
+  title: {
+    default: "NADD Living Boutique",
+    template: `%s | NADD Living Boutique`,
+  },
+  description: "Fabricantes de muebles y sillones de primera calidad",
+  openGraph: {
+    title: {
+      default: "NADD Living Boutique",
+      template: `%s | NADD Living Boutique`,
+    },
+    description: "Fabricantes de muebles y sillones de primera calidad",
+  },
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={`overflow-x-hidden ${montserrat.variable} ${cormorant.variable}`}
+    >
+      <body className="overflow-x-hidden">
+        <main>
+          <Nav />
+          {children}
+          <Footer />
+          <BackToTop />
+        </main>
+      </body>
+    </html>
+  );
 }
--- src/app/(client)/productos/[product]/page.tsx
+++ src/app/(client)/productos/[product]/page.tsx
@@ -6,13 +6,37 @@ type Props = {
   params: { product: "string" };
 };
 
+export async function generateMetadata({ params }: Props) {
+  try {
+    const { product } = params;
+
+    const { name, images, category, description } = await getProduct(product);
+
+    if (!name) throw Error("product not found");
+    return {
+      title: `${category[0].toLocaleUpperCase() + category.slice(1)} ${name}`,
+      alternates: {
+        canonical: `/productos/${product}`,
+      },
+      openGraph: {
+        title: `${category} ${name}`,
+      },
+    };
+  } catch (error) {
+    return {
+      title: "Not Found",
+      description: "The page you are looking for does not exist",
+    };
+  }
+}
+
 export default async function page({ params }: Props) {
   const slug = params.product;
   const { name, images, category, description } = await getProduct(slug);
 
   return (
-    <div className="flex flex-col md:flex-row h-fit py-10 justify-around items-center mt-28">
-      <div className="w-[80vw] md:w-[40vw]">
+    <div className="flex flex-col md:flex-row h-fit py-10 justify-around items-center mt-[5.5rem]">
+      <div className="w-[80vw] md:w-[42vw]">
         <Carousel images={images} />
       </div>
       <div className="w-[90vw] md:w-[40vw] flex flex-col justify-center space-y-5">
