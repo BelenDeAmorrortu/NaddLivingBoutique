e2b966150f7478fe43f98446c1436e0d2523eee9
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2023-07-06 13:28:29 -0300

    fix: hide pagination when products are loading

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,6 +8,7 @@ type Props = {
   productsPerPage: number;
   current: number;
   setCurrent: (arg: number) => void;
+  visible: boolean;
 };
 
 export default function Pagination({
@@ -15,6 +16,7 @@ export default function Pagination({
   productsPerPage,
   current,
   setCurrent,
+  visible,
 }: Props) {
   const pages = [];
   const numOfPages = Math.ceil(products / productsPerPage);
@@ -24,7 +26,11 @@ export default function Pagination({
   }
 
   return (
-    <div className="flex space-x-5">
+    <div
+      className={"flex-row-center space-x-4 ".concat(
+        visible ? "visible" : "invisible"
+      )}
+    >
       <ArrowSmallLeftIcon
         onClick={() => setCurrent(current !== 1 ? current - 1 : current)}
         className="stroke-black hover:stroke-black-hover w-5 h-5"
@@ -49,3 +55,62 @@ export default function Pagination({
     </div>
   );
 }
+// import { navigation } from "@/utils/navigation";
+// import {
+//   ArrowSmallRightIcon,
+//   ArrowSmallLeftIcon,
+// } from "@heroicons/react/24/outline";
+// import { RouteHandlerManager } from "next/dist/server/future/route-handler-managers/route-handler-manager";
+// import { useRouter, useSearchParams } from "next/navigation";
+
+// type Props = {
+//   products: number;
+//   productsPerPage: number;
+//   handleChange: (button: string, value: number) => void;
+// };
+//
+// export default function Pagination({
+//   products,
+//   productsPerPage,
+//   handleChange,
+// }: Props) {
+//   const params = useSearchParams();
+//   const router = useRouter();
+//   const pages = [];
+//   const numOfPages = Math.ceil(products / productsPerPage);
+//   const current = !params.getAll("page").length
+//     ? 1
+//     : Number(params.getAll("page")[0]);
+
+//   for (let i = 1; i <= numOfPages; i++) {
+//     pages.push(i);
+//   }
+
+//   return (
+//     <div className="flex space-x-5">
+//       <ArrowSmallLeftIcon
+//         onClick={() => {
+//           if (current !== 1) handleChange("pagination", current - 1);
+//         }}
+//         className="stroke-black hover:stroke-black-hover w-5 h-5"
+//       />
+//       {pages.map((p) => (
+//         <button
+//           onClick={() => handleChange("pagination", p)}
+//           className={
+//             "bg-transparent border-none font-semi-bold hover:font-bold" +
+//             (current === p ? " text-red" : " text-black")
+//           }
+//         >
+//           {p}
+//         </button>
+//       ))}
+//       <ArrowSmallRightIcon
+//         onClick={() => {
+//           if (current !== pages.length) handleChange("pagination", current + 1);
+//         }}
+//         className="stroke-black hover:stroke-black-hover w-5 h-5"
+//       />
+//     </div>
+//   );
+// }
