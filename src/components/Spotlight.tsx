d2901289db285092f6399217d1f10db3d614c16c
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2023-07-06 14:00:41 -0300

    refactor: navigation util implemented

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { getSpotlight } from "../../sanity/sanity-utils";
 import Card from "./Card";
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { navigation } from "@/utils/navigation";
 
 export default async function Spotlight() {
   let products = await getSpotlight();
@@ -12,13 +13,13 @@ export default async function Spotlight() {
       <h3 className="title-2 m-3 mb-16">Destacados</h3>
       <div className="mb-7 flex-center flex-wrap w-[95%] md:w-[80%]">
         {products.length > 0 ? (
-          products.map((p) => <Card {...p} />)
+          products.map((p) => <Card {...p} key={p._id} />)
         ) : (
           <p>There are no products</p>
         )}
       </div>
       <Link
-        href="/productos"
+        href={navigation.productos}
         className="flex-row-center text-red font-bold hover:underline underline-offset-4 cursor-pointer"
       >
         Ver catálogo completo <ChevronRightIcon className="w-5 h-5 ml-2" />
