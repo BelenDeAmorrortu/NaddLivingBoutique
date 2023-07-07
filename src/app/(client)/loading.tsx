25a7678142aebbe9041ea1dc4f6c60cf9de63913
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2023-07-07 19:52:32 -0300

    fix: horizontal scroll

--- src/app/(client)/loading.tsx
+++ src/app/(client)/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "@/components";
 
 export default function loading() {
   return (
-    <div className="bg-black w-screen h-screen flex-col-center space-y-12">
+    <div className="bg-black w-full h-screen flex-col-center space-y-12">
       <Loader color="white" size="large" />
     </div>
   );
