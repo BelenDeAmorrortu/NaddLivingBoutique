b8ef33c5c74894058a20a646efb80a4d15045abd
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2023-07-23 19:06:13 -0300

    styles: make nav thicker on medium viewports

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import NavMd from "./NavMd";
 
 export default function Nav() {
   return (
-    <nav className="z-20 bg-black w-full h-16 sm:h-fit fixed top-0 left-0 flex-col-center text-sm">
+    <nav className="z-20 bg-black w-full h-16 lg:h-fit fixed top-0 left-0 flex-col-center text-sm">
       <NavLg />
       <NavMd />
     </nav>
