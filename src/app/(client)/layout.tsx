be5769dfdc41a088b70c7889fab12e099882dc43
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2023-10-29 20:23:06 -0300

    refactor: change layouts structure in order to not repeat code

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,52 +1,17 @@
 import { Nav, Footer, BackToTop } from "../../components";
-import { Montserrat, Cormorant_Garamond } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 
-const montserrat = Montserrat({
-  weight: ["300", "400", "600", "700"],
-  style: ["normal", "italic"],
-  variable: "--font-primary",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const cormorant = Cormorant_Garamond({
-  weight: ["300", "400", "600", "700"],
-  style: ["normal", "italic"],
-  variable: "--font-secondary",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-export const metadata = {
-  metadataBase: new URL("https://naddlivingboutique.com"),
-  title: {
-    default: "NADD Living Boutique",
-    template: `%s | NADD Living Boutique`,
-  },
-  description: "Fabricantes de muebles y sillones de primera calidad",
-  openGraph: {
-    title: {
-      default: "NADD Living Boutique",
-      template: `%s | NADD Living Boutique`,
-    },
-    description: "Fabricantes de muebles y sillones de primera calidad",
-  },
-};
-
-export default async function RootLayout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${montserrat.variable} ${cormorant.variable}`}>
-      <body>
-        <Nav />
-        <main>{children}</main>
-        <Footer />
-        <BackToTop />
-      </body>
-    </html>
+    <>
+      <Nav />
+      <main>{children}</main>
+      <Footer />
+      <BackToTop />
+    </>
   );
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import { Montserrat, Cormorant_Garamond } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  weight: ["300", "400", "600", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-primary",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const cormorant = Cormorant_Garamond({
+  weight: ["300", "400", "600", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-secondary",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  metadataBase: new URL("https://naddlivingboutique.com"),
+  title: {
+    default: "NADD Living Boutique",
+    template: `%s | NADD Living Boutique`,
+  },
+  description: "Fabricantes de muebles y sillones de primera calidad",
+  openGraph: {
+    title: {
+      default: "NADD Living Boutique",
+      template: `%s | NADD Living Boutique`,
+    },
+    description: "Fabricantes de muebles y sillones de primera calidad",
+  },
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${montserrat.variable} ${cormorant.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
